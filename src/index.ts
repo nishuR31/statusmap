2206410a00223c6974aca51ab5be8b12616f7edf
nishuR31 <nishangithub@gmail.com>
2025-11-02 19:51:38 +0530

    added string param for msg()

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ const reverseMap = Object.fromEntries(
   Object.entries(codes).map(([key, value])=> [value, key])
 );
 
-export const msg: (T: number) => string = (code) =>
+export const msg: (T: number|string) => string = (code) =>
   reverseMap[code] ?? "UnknownStatuscode";
 export const code: (T: string) => number | string = (name) =>
-  codes[name] ?? "UnknownStatusMessage";
+  codes[name]  ?? "UnknownStatusMessage";
 
 export default {
   ...codes,
